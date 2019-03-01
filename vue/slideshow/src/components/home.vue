866e47e6b01cab9188a9d11791931e4b3dd165d0
Tumbleweeeed <916539494@qq.com>
2019-03-01 10:53:43 +0800

    vue

---
+++ vue/slideshow/src/components/home.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="wrapper">
+		<div class="contain">
+			
+		</div>
+		<div class="dots">
+			<span class="dot"></span>
+			<span class="dot"></span>
+			<span class="dot"></span>
+			<span class="dot"></span>
+			<span class="dot"></span>
+			<span class="dot"></span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name:"Home",
+	data(){
+		return{
+			img:[""]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	.wrapper
+		width 700px
+		height 300px
+		top 20px
+		margin 80px auto
+		background red
+</style>
