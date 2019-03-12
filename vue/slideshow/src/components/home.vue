ee5489894ea23fc83f57e0dc6572e41c64787537
Tumbleweeeed <916539494@qq.com>
2019-03-12 23:15:45 +0800

    dot&slide

--- vue/slideshow/src/components/home.vue
+++ vue/slideshow/src/components/home.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="wrapper">
 		<div class="arrow">
-			<div class="left"></div>
-			<div class="right"></div>
+			<div class="left" @click="move(1)"></div>
+			<div class="right" @click="move(-1)"></div>
 		</div>
 		<div class="contain">
-			<div class="show">
+			<div class="show" :style="containStyle">
 				<li>
 					<img src="../../static/img/5.jpg" alt="">
 				</li>
-				<li v-for="item in img" :key="index">
+				<li v-for="(item,index) in img" :key="index">
 					<img :src="item" alt="">
 				</li>
 				<li>
@@ -18,11 +18,13 @@
 			</div>
 		</div>
 		<div class="dots">
-			<span class="dot"></span>
-			<span class="dot"></span>
-			<span class="dot"></span>
-			<span class="dot"></span>
-			<span class="dot"></span>
+			<span 
+				v-for="(dots,i) in img"
+				:key="i"
+				:class="{dotted: i === (currentIndex-1)}"
+				class="dot"
+			>
+			</span>
 		</div>
 	</div>
 </template>
@@ -32,7 +34,25 @@ export default {
 	name:"Home",
 	data(){
 		return{
-			img:["../../static/img/1.jpg","../../static/img/2.jpg","../../static/img/3.jpg","../../static/img/4.jpg","../../static/img/5.jpg"]
+			img:["../../static/img/1.jpg","../../static/img/2.jpg","../../static/img/3.jpg","../../static/img/4.jpg","../../static/img/5.jpg"],
+			distance:-100,
+			currentIndex:1
+		}
+	},
+	computed:{
+		containStyle(){
+			return{
+				left:`${this.distance}%`
+			}
+		}
+	},
+	methods:{
+		move(direct){
+			direct == -1 ? this.currentIndex++ : this.currentIndex--
+			console.log(this.currentIndex)
+			this.distance += direct*100
+			if(this.distance<-500)this.distance=-100;this.currentIndex=1
+			if(this.distance>-100)this.distance=-500;this.currentIndex=5
 		}
 	}
 }
