2f5b03f2a4cec0a5f708c61d9f61167954919dd5
Tumbleweeeed <916539494@qq.com>
2019-03-13 13:35:00 +0800

    finish

--- vue/slideshow/src/components/home.vue
+++ vue/slideshow/src/components/home.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="wrapper">
 		<div class="arrow">
-			<div class="left" @click="move(1)"></div>
-			<div class="right" @click="move(-1)"></div>
+			<div class="left" @click="move(1,100)"></div>
+			<div class="right" @click="move(-1,100)"></div>
 		</div>
 		<div class="contain">
 			<div class="show" :style="containStyle">
@@ -23,6 +23,7 @@
 				:key="i"
 				:class="{dotted: i === (currentIndex-1)}"
 				class="dot"
+				@click="jump(i+1)"
 			>
 			</span>
 		</div>
@@ -34,9 +35,10 @@ export default {
 	name:"Home",
 	data(){
 		return{
-			img:["../../static/img/1.jpg","../../static/img/2.jpg","../../static/img/3.jpg","../../static/img/4.jpg","../../static/img/5.jpg"],
+			img:["./static/img/1.jpg","./static/img/2.jpg","./static/img/3.jpg","./static/img/4.jpg","./static/img/5.jpg"],
 			distance:-100,
-			currentIndex:1
+			currentIndex:1,
+			doorKey:true
 		}
 	},
 	computed:{
@@ -47,17 +49,116 @@ export default {
 		}
 	},
 	methods:{
-		move(direct){
-			direct == -1 ? this.currentIndex++ : this.currentIndex--
-			console.log(this.currentIndex)
-			this.distance += direct*100
-			if(this.distance<-500)this.distance=-100;this.currentIndex=1
-			if(this.distance>-100)this.distance=-500;this.currentIndex=5
+		move(direct,distance){
+			if(!this.doorKey)return
+			this.doorKey=false
+			direct == -1 ? this.currentIndex+=distance/100 : this.currentIndex-=distance/100
+			if(this.currentIndex>5)this.currentIndex=1
+			if(this.currentIndex<1)this.currentIndex=5
+			const initdes = this.distance+direct*distance
+			this.animate(initdes,direct)
+		},
+		animate(des,direct){
+			if(direct == -1 && des < this.distance || direct == 1 && des > this.distance){
+				this.distance += 2*direct
+				window.setTimeout(()=>{
+					this.animate(des,direct)
+				},4)
+			}else{
+				this.doorKey=true
+				this.distance = des
+				if(this.distance<-500)this.distance=-100
+				if(this.distance>-100)this.distance=-500
+			}
+		},
+		jump(index){
+			const direct = index-this.currentIndex>=0 ? -1 : 1
+			const offset = Math.abs(index-this.currentIndex)*100
+			this.move(direct,offset)
 		}
 	}
 }
 </script>
 
 <style lang="stylus" scoped>
-	@import '../../static/css/main.styl'
+.wrapper
+	position relative
+	width 700px
+	height 300px
+	top 20px
+	margin 80px auto
+	background rgba(0,0,1,.1)
+	&:hover .arrow
+		opacity 1
+	.contain
+		position absolute
+		top 0
+		left 0
+		right 0
+		bottom 40px
+		overflow hidden
+		.show
+			position relative
+			width 4900px
+			height 260px
+			left -100%
+			li
+				margin 0
+				float left
+				width 700px
+				height 260px
+				list-style none
+				img
+					margin 0
+					width 700px
+					height 260px
+	.dots
+		position absolute
+		bottom 5px
+		margin-top:-10px;
+		width:200px;
+		height:20px;
+		left:50%;
+		margin-left:-100px;
+		display:flex;
+		justify-content:space-around;
+		.dot
+			width:10px;
+			height:10px;
+			background:black;
+			border-radius:10px;
+			cursor:pointer;
+			&:hover
+				background rgba(0,0,0,.5)
+	.arrow
+		opacity 0
+		.left
+			position absolute
+			width 30px
+			height 70px
+			top 50%
+			left 0
+			margin-top -35px
+			background rgba(0,0,0,.6)
+			z-index 9
+			cursor pointer
+			&:hover
+				opacity .5
+		.right
+			position absolute
+			width 30px
+			height 70px
+			top 50%
+			right 0
+			margin-top -35px
+			background rgba(0,0,0,.6)
+			z-index 9
+			cursor pointer
+			&:hover
+				opacity .5
+.dotted
+	background gray !important
+
+
+
 </style> 
